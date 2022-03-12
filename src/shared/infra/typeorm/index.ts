f3ad41f2b9fc2bcda377efd475463d1d4ece778e
Paulo Henrique <phferreirasouza106@gmail.com>
2022-03-12 02:47:21 -0300

    adding database environment host

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,8 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
-export default async function getConnectionTypeOrm(host = "postgres_database") {
+export default async function getConnectionTypeOrm(
+  host = process.env.DATABASE_HOST
+) {
   const connectionOptions = await getConnectionOptions();
 
   const hostMapped = process.env.NODE_ENV === "test" ? "localhost" : host;
