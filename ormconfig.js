f2f8f197b38ddcf476b2278aad2740e42e9df75b
Paulo Henrique <phferreirasouza106@gmail.com>
2022-03-12 02:53:50 -0300

    fixing typeorm configs

--- ormconfig.js
+++ ormconfig.js
@@ -5,10 +5,18 @@ module.exports = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASS,
   database: process.env.DATABASE_NAME,
+  synchronize: true,
+  logging: true,
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
   entities: ["src/**/typeorm/entities/*.ts"],
   seeds: ["./src/shared/infra/typeorm/seed/*.ts"],
   cli: {
     migrationsDir: "./src/shared/infra/typeorm/migrations",
+  },
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
