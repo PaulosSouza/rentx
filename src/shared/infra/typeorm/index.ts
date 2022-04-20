0f5de50b2881e20d3cf8f75f63e2ffb3ca468ee2
Paulo Henrique Ferreira de Souza <paulo.souza@cubos.io>
2022-04-20 18:20:14 +0000

    refactor: fix connection of database depending of enviromnent type

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,32 +1,42 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
+export type Environment = "test" | "production";
+
 export default async function getConnectionTypeOrm() {
-  const environment = process.env.NODE_ENV;
+  const environment = process.env.NODE_ENV as Environment;
 
   const connectionOptions = await getConnectionOptions();
 
-  if (environment === "test") {
-    const { DATABASE_TEST_HOST, DATABASE_TEST_PORT, DATABASE_TEST_NAME } =
-      process.env;
+  const { DATABASE_TEST_HOST, DATABASE_TEST_PORT, DATABASE_TEST_NAME } =
+    process.env;
 
-    Object.assign(connectionOptions, {
-      host: DATABASE_TEST_HOST,
-      port: DATABASE_TEST_PORT,
-      database: DATABASE_TEST_NAME,
-    });
-  }
+  switch (environment) {
+    case "test":
+      Object.assign(connectionOptions, {
+        host: DATABASE_TEST_HOST,
+        port: DATABASE_TEST_PORT,
+        database: DATABASE_TEST_NAME,
+      });
+      break;
 
-  if (environment === "production") {
-    Object.assign(connectionOptions, {
-      migrationsRun: true,
-      logger: true,
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
+    case "production":
+      Object.assign(connectionOptions, {
+        logging: true,
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
-      },
-    });
+      });
+      break;
+
+    default:
+      Object.assign(connectionOptions, {
+        migrationsRun: true,
+        logging: true,
+      });
+      break;
   }
 
   const connection = await createConnection(connectionOptions);
