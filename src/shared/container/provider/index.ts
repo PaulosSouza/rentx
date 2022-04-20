aa8279355a686746d22d49a1552fa48715c63ae2
Paulo Henrique Ferreira de Souza <paulo.souza@cubos.io>
2022-04-20 18:19:08 +0000

    feat: add upload provider

--- src/shared/container/provider/index.ts
+++ src/shared/container/provider/index.ts
@@ -4,10 +4,17 @@ import DayjsDateProvider from "./DateProvider/implementations/DayjsDateProvider"
 import IDateProvider from "./DateProvider/interfaces/IDateProvider";
 import EtherealMailProvider from "./MailProvider/implementatinos/EtherealMailProvider";
 import IMailProvider from "./MailProvider/interfaces/IMailProvider";
+import LocalStorageProvider from "./StorageProvider/implementations/LocalStorageProvider";
+import IStorageProvider from "./StorageProvider/interfaces/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
 
 container.registerSingleton<IMailProvider>(
   "MailProvider",
   EtherealMailProvider
+);
+
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider",
+  LocalStorageProvider
 );
